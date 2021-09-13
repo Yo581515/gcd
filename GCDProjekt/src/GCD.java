3b88f8783c70b5c80fcc5bfd6ab9151b801bce99
581515 <47955690+Y581515@users.noreply.github.com>
2021-09-13 02:52:09 +0200

    gcd fin

--- GCDProjekt/src/GCD.java
+++ GCDProjekt/src/GCD.java
@@ -1,4 +1,4 @@
- 
+import java.util.Collections;
 
 public class GCD {
 
@@ -8,6 +8,7 @@ public class GCD {
 		int b = 3_524_578;
 		int gcd = GCDMetode(a, b);
 
+		System.out.println();
 		System.out.println("Stoorste Felles Divsor\ngcd(" + a + ", " + b + ") = " + gcd);
 
 	}
@@ -17,16 +18,68 @@ public class GCD {
 		int min = Math.min(a, b);
 
 		int rest = max;
-
+		int del;
 		while (rest != 0) {
-			int del = max / min;
+			String mellomrom1 = tallLengde(max);
+			String mellomrom2 = tallLengde(min);
+			del = max / min;
 			rest = max % min;
-			System.out.println(max + " = " + del + "*" + min + " + " + rest);
+			System.out.println(max + mellomrom1 + " = " + del + "*" + min + mellomrom2 + " + " + rest);
 			max = min;
 			min = rest;
 		}
 
 		return max;
+	}
+
+	private static String tallLengde(int tall) {
+		int length = (int) (Math.log10(tall) + 1);
+		String mellomrom;
+		switch (length) {
+		case 1:
+			mellomrom = "          ";
+			break;
+		case 2:
+			mellomrom = "         ";
+			;
+			break;
+		case 3:
+			mellomrom = "        ";
+			;
+			break;
+		case 4:
+			mellomrom = "       ";
+			;
+			break;
+		case 5:
+			mellomrom = "      ";
+			;
+			break;
+		case 6:
+			mellomrom = "     ";
+			;
+			break;
+		case 7:
+			mellomrom = "    ";
+			;
+			break;
+		case 8:
+			mellomrom = "   ";
+			;
+			break;
+		case 9:
+			mellomrom = "  ";
+			;
+			break;
+		case 10:
+			mellomrom = " ";
+			break;
+		default:
+			mellomrom = "";
+			break;
+		}
+
+		return mellomrom;
 	}
 
 }
